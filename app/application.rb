32c40ede1ffbfeb8454b629d1101d3d5688468f9
Lucy Wall <lhwall@gmail.com>
2018-05-23 01:19:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,8 +5,8 @@ class Application
     if Time.new.hour < 12
       resp.write "Good Morning!"
     elsif Time.new.hour >= 12
-      resp.write "Good Afternoon!"
       binding.pry
+      resp.write "Good Afternoon!"
     end
     resp.finish
   end
