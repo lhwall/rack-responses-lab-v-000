0a2c927ed79f0acf129c704b9f1c6fd6e6614f23
Lucy Wall <lhwall@gmail.com>
2018-05-23 01:23:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,15 +1,14 @@
-require 'pry'
 class Application
 
   def call(env)
   binding.pry
-    # resp = Rack::Response.new
-    # if Time.new.hour < 12
-    #   resp.write "Good Morning!"
-    # elsif Time.new.hour >= 12
-    #   resp.write "Good Afternoon!"
-    # end
-    # resp.finish
+    resp = Rack::Response.new
+    if Time.new.hour < 12
+      resp.write "Good Morning!"
+    elsif Time.new.hour >= 12
+      resp.write "Good Afternoon!"
+    end
+    resp.finish
   end
 
 end
